73bffc387cc5043d09a6fb2bb0ed3db82998965d
chenlu <1406152217@qq.com>
2020-08-28 23:39:36 +0800

    修改随机生成邮箱地址前缀长度为6-18位

--- wdkf-spring-boot-utils/src/main/java/com/wdkf/wdkfspringbootutils/random/RandomUtils.java
+++ wdkf-spring-boot-utils/src/main/java/com/wdkf/wdkfspringbootutils/random/RandomUtils.java
@@ -514,7 +514,8 @@ public class RandomUtils {
      */
     public static String randomEmail(){
         StringBuilder result = new StringBuilder();
-        result.append(randomNumbersAndLowercase(10));
+        int num = new Random().nextInt(12)+6;
+        result.append(randomNumbersAndLowercase(num));
         String[] suffix = {"@gmail.com","@yahoo.com","@msn.com","@hotmail.com","@aol.com","@ask.com",
         "@live.com","@qq.com","@0355.net","@163.com","@163.net","@263.net","@3721.net","@yeah.net","@googlemail.com","@mail.com"};
         int random = new Random().nextInt(suffix.length);
